ef1a8d1abbb2645f571a411f1ad3d9910bdf7e71
Dominat0rr <claermandominik@gmail.com>
2020-03-09 16:07:24 +0100

    Added option to delete and add transactions
    Made states to addTransaction and deleteTransaction, update income and expenses depending on the transactions

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -3,13 +3,17 @@ import AppReducer from './AppReducer';
 
 // Initial state
 const initialState = {
-    transactions:  [
-        { id: 1, text: 'Flower', amount: -20 },
-        { id: 2, text: 'Salary', amount: 300 },
-        { id: 3, text: 'Book', amount: -10 },
-        { id: 4, text: 'Camera', amount: 150 }
-    ]
+    transactions:  []
 };
+
+// const initialState = {
+//     transactions:  [
+//         { id: 1, text: 'Flower', amount: -20 },
+//         { id: 2, text: 'Salary', amount: 300 },
+//         { id: 3, text: 'Book', amount: -10 },
+//         { id: 4, text: 'Camera', amount: 150 }
+//     ]
+// };
 
 // Create context
 export const GlobalContext = createContext(initialState);
@@ -18,8 +22,27 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
+    // Actions
+    function deleteTransaction(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        });
+    };
+
+    function addTransaction(transaction) {
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        });
+    }
+
     return (
-        <GlobalContext.Provider value={{ transactions: state.transactions }}>
+        <GlobalContext.Provider value={{ 
+            transactions: state.transactions,
+            deleteTransaction,
+            addTransaction
+            }}>
             { children }
         </GlobalContext.Provider>
     );
